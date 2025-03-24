1aec14db71ae778958eb30d15b4b753fff1c8e2e
05derek <derek.dzakir.ft24@mail.umy.ac.id>
2025-03-24 13:11:25 +0700

    step 1,2

--- InsertionSort.cpp
+++ InsertionSort.cpp
@@ -1,26 +1,11 @@
 
- void input()
+void insertionSort()
 {
-cout << "masukkan jumlah data pada Array :";
-cin >> n;
 
-if (n <= 20)
-{
-break;
-}
-else
-{
-cout << "n\Array yang anda masukkan maksimal 20 elemen.\n";
-}
-}
-cout << endl;
-cout << "========================" << endl;
-cout << "masukkan element Array" << endl;
-cout << "========================" << endl;
+int temp;
+int j, i;
 
-for (int i = 0; i < n; i++)
-{
-cout << "data ke-" << (i=1) << ":";
-cin >> arr[i];
-}
-}
+for (i = 1; i <= n - 1; i++)
+{ // step 1
+
+temp = arr[i]; // step 2
